9fff6c186078cd3bf835f786e337612e34a9feee
하지민 <tracygkwlals@sookmyung.ac.kr>
2025-02-21 16:48:06 +0900

    [FEAT] day15 미션 제출

---
+++ missions/day15/src/App.jsx
@@ -0,0 +1,50 @@
+import "./App.css";
+import { useRef, useReducer } from "react";
+import ContactEditor from "./components/ContactEditor";
+import ContactList from "./components/ContactList";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [...state, action.data];
+    case "DELETE":
+      return state.filter((contact) => contact.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
+export default function App() {
+  const [contacts, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  const onCreate = (name, contact) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        name,
+        contact,
+      },
+    });
+  };
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: "DELETE",
+      targetId,
+    });
+  };
+
+  return (
+    <div className="App">
+      <h2>Contact List</h2>
+      <section>
+        <ContactEditor onCreate={onCreate} />
+      </section>
+      <section>
+        <ContactList contacts={contacts} onDelete={onDelete} />
+      </section>
+    </div>
+  );
+}
